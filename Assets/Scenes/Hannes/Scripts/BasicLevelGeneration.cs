4cfe2852df1eb9ce74617f2b2b23ac4bdb2687d2
Prototype113 <hann3s.malfait@gmail.com>
2025-04-23 18:33:49 +0200

    added extra level
    Added extra level and made scene for the creation of inventory system. Didn't manage to make anything in there yet though.

--- Assets/Scenes/Hannes/Scripts/BasicLevelGeneration.cs
+++ Assets/Scenes/Hannes/Scripts/BasicLevelGeneration.cs
@@ -7,12 +7,13 @@ public class BasicLevelGeneration : MonoBehaviour
     public GameObject groundLevel;
     public GameObject level1;
     public GameObject level2;
+    public GameObject level3;
     //put in move levels when more designed
     public GameObject roof;
     public int floorAmount;
 
 
-    private int levelAmount = 2; //put in the amount of designed levels appart from ground level
+    private int levelAmount = 3; //put in the amount of designed levels appart from ground level
     private float locationHeight = 0f; //don't change unless you want to recreate UP
 
 
@@ -49,8 +50,21 @@ public class BasicLevelGeneration : MonoBehaviour
                 }
                 locationHeight += 2.7f; //floorheight is 270cm
             }
+            else if ((Random.Range(0, levelAmount)) == 2)
+            {
+                if (i % 2 == 0)
+                {
+                    Instantiate(level3, new Vector3(0, locationHeight, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                }
+                else
+                {
+                    Instantiate(level3, new Vector3(0, locationHeight, 0), Quaternion.identity);
+                }
+                locationHeight += 2.7f; //floorheight is 270cm
+            }
             //copy paste 'else if' here to add more floors
             //change 'levelx' to levelname
+            //raise treshhold in else if statement by one
 
             if (i == (floorAmount - 1)) //adding roof
             {
