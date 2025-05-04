2427f3a18295f21c8ac0e4a93a3e22f645c94b01
Noah <noahdmx@gmail.com>
2025-05-04 21:44:54 +0200

    Fixed level gen bug
    Made copy of "basic level generator" and named it "lvlGen", all I changed was make it use a public array of gameobjects instead of individual gameobject variables.
    
    Somehow that fixed it magically, didn't need to even touch the part that rotates the object.

---
+++ Assets/Scenes/Noah/Scripts/lvlGen.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class lvlGen : MonoBehaviour
+{
+
+
+    public GameObject groundLevel;
+    public GameObject[] levels;
+    //put in move levels when more designed
+    public GameObject roof;
+    public int floorAmount;
+
+
+    private int levelAmount = 3; //put in the amount of designed levels appart from ground level
+    private float locationHeight = 0f; //don't change unless you want to recreate UP
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        Instantiate(groundLevel, new Vector3(0, 0, 0), Quaternion.identity);
+        locationHeight += 3f; //groundfloor is 300cm
+
+        for (int i = 0; i < floorAmount; i++)
+        {
+            int level = (Random.Range(0, levelAmount));
+            {
+                if (i % 2 == 0)
+                {
+                    Instantiate(levels[level], new Vector3(0, locationHeight, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                }
+                else
+                {
+                    Instantiate(levels[level], new Vector3(0, locationHeight, 0), Quaternion.identity);
+                }
+                locationHeight += 2.7f; //floorheight is 270cm
+            }
+
+            //add new levels to the public "levels" array
+            //change 'levelx' to levelname
+            //raise treshhold in else if statement by one
+
+            if (i == (floorAmount - 1)) //adding roof
+            {
+                if (i % 2 == 0)
+                {
+                    Instantiate(roof, new Vector3(0, locationHeight, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(roof, new Vector3(0, locationHeight, 0), Quaternion.Euler(new Vector3(0, 180, 0)));
+                }
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
