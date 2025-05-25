48f6b497b8ad75307c91ffa9ba8427c8646b3ef2
CeaGames <101206227+CeaGames@users.noreply.github.com>
2025-05-25 09:55:12 +0200

    ZombiesSpreadOut

--- Assets/Scenes/Combined/Scripts/CeAiBehaviourScriptCopy.cs
+++ Assets/Scenes/Combined/Scripts/CeAiBehaviourScriptCopy.cs
@@ -7,33 +7,60 @@ public class CeAiBehaviourScriptCopy : MonoBehaviour
     [SerializeField] private Transform _playerT;
     [SerializeField] private NavMeshAgent _zobmieAgent;
 
-    public List<Transform> _doors = new List<Transform>();
-    public List<Transform> _totalDoors = new List<Transform>();
+    [SerializeField] private float offsetUpdateInterval = 4f;
+    [SerializeField] private float offsetRange = 5f;
+    [SerializeField] private float directChaseDistance = 3f;
 
-    private Transform _nearestDoor = null;
-    float _shortestDoorDistance = float.MaxValue;
+    private Vector3 currentOffset;
+    private float offsetTimer;
 
-    private float timer = 10;
+    private float timeBeforeStart = 5;
 
     void Start()
     {
-
+        UpdateOffset(); // Initialize first offset
+        offsetTimer = offsetUpdateInterval;
     }
 
     void Update()
     {
-        timer -= Time.deltaTime;
+        timeBeforeStart -= Time.deltaTime;
 
-        if(timer < 0)
+        // Update the random offset every few seconds
+        if(timeBeforeStart <= 0)
         {
-            if (IsPathReachable(_playerT.position))
+            offsetTimer -= Time.deltaTime;
+
+            if (offsetTimer <= 0)
             {
-                _zobmieAgent.SetDestination(_playerT.position);
+                UpdateOffset();
+                offsetTimer = offsetUpdateInterval;
+            }
+
+            Vector3 destination;
+
+            // If close enough to the player, use direct path
+            float distanceToPlayer = Vector3.Distance(transform.position, _playerT.position);
+            if (distanceToPlayer <= directChaseDistance)
+            {
+                destination = _playerT.position;
+            }
+            else
+            {
+                destination = _playerT.position + currentOffset;
+            }
+
+            if (IsPathReachable(destination))
+            {
+                _zobmieAgent.SetDestination(destination);
             }
         }
     }
 
-
+    void UpdateOffset()
+    {
+        currentOffset = new Vector3(Random.Range(-offsetRange, offsetRange), 0, Random.Range(-offsetRange, offsetRange));
+    }
 
     bool IsPathReachable(Vector3 targetPosition)
     {
@@ -41,46 +68,4 @@ public class CeAiBehaviourScriptCopy : MonoBehaviour
         _zobmieAgent.CalculatePath(targetPosition, path);
         return path.status == NavMeshPathStatus.PathComplete;
     }
-
-    bool IsDoorReachable(Vector3 doorPosition)
-    {
-        float checkRadius = 1f; // You can adjust this value
-        int checkPoints = 8; // Number of directions to test
-
-        for (int i = 0; i < checkPoints; i++)
-        {
-            float angle = i * Mathf.PI * 2f / checkPoints;
-            Vector3 offset = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * checkRadius;
-            Vector3 checkPos = doorPosition + offset;
-
-            NavMeshPath path = new NavMeshPath();
-            _zobmieAgent.CalculatePath(checkPos, path);
-
-            if (path.status == NavMeshPathStatus.PathComplete)
-                return true;
-        }
-
-        return false;
-    }
-
-    void GetNearestDoor()
-    {
-        _shortestDoorDistance = float.MaxValue;
-        _nearestDoor = null;
-
-        // Remove inactive or null doors
-        _doors.RemoveAll(door => door == null || !door.gameObject.activeSelf);
-
-        foreach (Transform door in _doors)
-        {
-            float distance = Vector3.Distance(transform.position, door.position);
-
-            if (distance < _shortestDoorDistance && IsDoorReachable(door.position))
-            {
-                _shortestDoorDistance = distance;
-                _nearestDoor = door;
-            }
-        }
-    }
-
 }
