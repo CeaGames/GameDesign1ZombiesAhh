7d9c4c9f882e0dab275804d6ca42bf3271bf88c6
CeaGames <101206227+CeaGames@users.noreply.github.com>
2025-06-05 16:52:32 +0200

    AddedControllsScreen

--- Assets/Scenes/Combined/Scripts/SettingsCarrier.cs
+++ Assets/Scenes/Combined/Scripts/SettingsCarrier.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class SettingsCarrier
 {
-    public static float mouseSensetivity;
+    public static float mouseSensetivity = 0.3f * 1000f;
     public static int levelAmount;
 
     public static bool SettingsHaveStarted = false;
